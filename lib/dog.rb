0bebd37a7be0cfb498012941b4a9458a083e5eb1
karen ercoli <karenercoli@gmail.com>
2018-11-14 11:49:46 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,12 +1,12 @@
 class Dog
-  def name(name = "Fido")
-    @name = name
+  def name(name= "Fido")
+    @name= name
   end
   def name
     @name
   end
   def bark(bark)
-    @bark = bark
+    @bark= bark
   end
   def bark
     @bark
